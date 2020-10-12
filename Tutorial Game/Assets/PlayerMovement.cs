5165fb019f4ef154d34ad5f0c42d065ce315f241
HankArt-dev <hank@hankart.com>
2020-10-11 22:27:06 -0500

    EP03 MOVEMENT Completed
    yay

--- Tutorial Game/Assets/PlayerMovement.cs
+++ Tutorial Game/Assets/PlayerMovement.cs
@@ -5,16 +5,50 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    public float forwardForce = 500f;
+    public float moveSpeed = 500f;
+    bool moveRight = false;
+    bool moveLeft = false;
+
+
+
+
+    void Update()
     {
-        
+        if (Input.GetKey("d"))
+        {
+            moveRight = true;
+        }
+        else
+        {
+            moveRight = false;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            moveLeft = true;
+        }
+        else
+        {
+            moveLeft = false;
+        }
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (moveRight == true)
+        {
+            rb.AddForce(moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (moveLeft == true)
+        {
+            rb.AddForce(-moveSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
