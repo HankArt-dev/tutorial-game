37d513989ec42cfa7370c8d38ff2ecf5284723bb
HankArt-dev <hank@hankart.com>
2020-10-11 23:05:07 -0500

    EP04 CAMERA FOLLOW Complete
    I did it!

---
+++ Tutorial Game/Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform ground;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+
+        if (player.position.y <= -50)
+        {
+            transform.LookAt(ground);
+        }
+        
+    }
+}
