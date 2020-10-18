2e35a639e18f5b953a1ca78934f911080fb47cc4
HankArt-dev <hank@hankart.com>
2020-10-18 15:38:43 -0500

    hope

--- Tutorial Game/Assets/GameManager.cs
+++ Tutorial Game/Assets/GameManager.cs
@@ -9,6 +9,14 @@ public class GameManager : MonoBehaviour
 
     public float restartDelay = 1f;
 
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+	{
+        completeLevelUI.SetActive(true);
+	}
+
+
    public void EndGame ()
     {
         
